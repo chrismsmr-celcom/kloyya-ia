ff1881a14749a85f97b042a21f9fab0b3214464b
christopher Dikesa <chrismsmr@gmail.com>
2026-09-04 13:00:32 +0100

    Fix import statement in index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,1 @@
-from app.main import app
-
-# Vercel utilise 'app' comme handler
-# C'est tout ce qu'il faut pour les serverless functions
-
-# Pour les routes, on importe le main qui inclut tous les routers
+rom app.main import app
